f6e7496033d6c5b46001fec3328c87df0816f75b
koussay557 <henidkousay@gmail.com>
2024-06-11 16:53:00 +0100

    Express Server Setup

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import router from './routes/index.js';
-import dotenv from 'dotenv';
-dotenv.config();
+const express = require('express');
+const router = require('./routes/index');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use('/', router);
+router(app);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
